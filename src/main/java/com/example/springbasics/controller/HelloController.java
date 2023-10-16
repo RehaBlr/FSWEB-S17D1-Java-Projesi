b5a2c035aedf37d7f63438308d1da3349ef63fde
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-10-16 22:32:42 +0300

    Done!

---
+++ src/main/java/com/example/springbasics/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springbasics.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${instructor.name}")
+    private String name;
+
+    @Value("${instructor.surname}")
+    private String surname;
+
+    @GetMapping("/hi")
+    public String sayHi(){
+        //name = "Burak";
+        return name + " " + surname + " says hello world";
+    }
+
+}
---
+++ src/main/java/com/example/springbasics/validation/AnimalValidation.java
@@ -0,0 +1,25 @@
+package com.example.springbasics.validation;
+
+import com.example.springbasics.entity.Animal;
+
+import java.util.Map;
+
+public class AnimalValidation {
+
+    public static boolean isIdValid(int id){
+        return !(id < 0);
+    }
+
+    public static boolean isAnimalContains(Map<Integer, Animal> animals, int id){
+        return animals.containsKey(id);
+    }
+
+    public static boolean isAnimalCredentialsValid(Animal animal){
+        return !(animal.getId() <= 0 ||
+                animal.getName() == null ||
+                animal.getName().isEmpty() ||
+                animal.getWeight() <= 0);
+    }
+
+
+}
